d3b0ced4599012680b9cb0bc19c04f7ad648d972
ANTIN <antonzhelihovskiy@gmail.com>
2025-09-29 13:43:32 +0300

    build udp server in to main

--- task1/main.py
+++ task1/main.py
@@ -4,6 +4,13 @@ import pathlib
 import mimetypes
 import socket
 import json
+from pathlib import Path
+from datetime import datetime
+import threading
+
+STORAGE = Path("storage")
+STORAGE.mkdir(exist_ok=True)
+DATA_FILE = STORAGE / "data.json"
 
 class HttpHandler(BaseHTTPRequestHandler):
     def do_POST(self):
@@ -18,15 +25,15 @@ class HttpHandler(BaseHTTPRequestHandler):
         sock.close()
 
         self.send_response(302)
-        self.send_header('Location', '/')
+        self.send_header('Location', '/message')
         self.end_headers()
 
     def do_GET(self):
         pr_url = urllib.parse.urlparse(self.path)
         if pr_url.path == '/':
             self.send_html_file('index.html')
-        elif pr_url.path == '/contact':
-            self.send_html_file('contact.html')
+        elif pr_url.path == '/message':
+            self.send_html_file('message.html')
         else:
             if pathlib.Path().joinpath(pr_url.path[1:]).exists():
                 self.send_static()
@@ -60,7 +67,39 @@ def run(server_class=HTTPServer, handler_class=HttpHandler):
     except KeyboardInterrupt:
         http.server_close()
 
+def run_udp_server(host="127.0.0.1", port=5000):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((host, port))
+    print(f"UDP сервер запущений {host}:{port}")
+
+    while True:
+        data, addr = sock.recvfrom(1024)
+        message = data.decode("utf-8")
+        print(f"Отримано від {addr}: {message}")
+
+        try:
+            data_dict = json.loads(message)
+        except json.JSONDecodeError:
+            continue
+
+        # Читаємо старі дані
+        if DATA_FILE.exists():
+            with open(DATA_FILE, "r", encoding="utf-8") as f:
+                old_data = json.load(f)
+        else:
+            old_data = {}
+
+        # Додаємо нові
+        date_stamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+        old_data[date_stamp] = data_dict
+
+        # Записуємо назад
+        with open(DATA_FILE, "w", encoding="utf-8") as f:
+            json.dump(old_data, f, ensure_ascii=False, indent=4)
 
 if __name__ == '__main__':
-    run()
+    t1 = threading.Thread(target=run)
+    t2 = threading.Thread(target=run_udp_server)
+    t1.start()
+    t2.start()
 
